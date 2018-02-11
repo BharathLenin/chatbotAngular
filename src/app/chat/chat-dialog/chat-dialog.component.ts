5336d9d0bb88d7e95fcacc3f094b6435c1afaa88
unknown <dineeshsz@gmail.com>
2018-02-11 15:26:43 +0530

    new style

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,5 +1,5 @@
 import { SpeechRecognitionService } from '../../speech-recognition.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ChatService, Message } from '../../chat.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/scan';
@@ -11,12 +11,19 @@ import 'rxjs/add/operator/scan';
 export class ChatDialogComponent implements OnInit {
   messages: Observable<Message[]>;
   formValue: string;
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+
   constructor(public chat: ChatService) { }
   ngOnInit() {
     // appends to array after each new message is added to feedSource
     this.messages = this.chat.conversation.asObservable()
         .scan((acc, val) => acc.concat(val) );
   }
+
+  ngAfterViewChecked() {  
+    this.myScrollContainer.nativeElement.scrollIntoView({behavior: "smooth", block: "end", inline: "end"});
+    //this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+} 
 
   
   
